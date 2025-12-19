66636cc3000b8daa2da1a966dc63bd576e3d8509
antoinealmoussa <antoine.almoussa@halias.fr>
2025-12-19 14:18:16 +0100

    Display full country name

--- back/api-name-origin-probabilities/src/api_name_origin_probabilities/main.py
+++ back/api-name-origin-probabilities/src/api_name_origin_probabilities/main.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
+from sqlalchemy.orm import Session
 from fastapi.middleware.cors import CORSMiddleware
 from src.api_name_origin_probabilities.schemas import ApiNameOriginProbabilities
 from src.api_name_origin_probabilities.services.api_nationalize import fetch_nationalize_api_data, remodel_data
+import src.api_name_origin_probabilities.models as models
+from src.api_name_origin_probabilities.database import Base, engine, get_db
+from src.api_name_origin_probabilities.country import router as country_routes
 
 app = FastAPI()
 
@@ -14,20 +18,24 @@ app.add_middleware(
     allow_headers=["*"],  # Autorise tous les en-têtes
 )
 
+models.Base.metadata.create_all(bind=engine)
+
 
 @app.get(
     "/name_probabilites/{name}",
     response_model=ApiNameOriginProbabilities
 )
-async def get_name_probabilities(name: str):
+async def get_name_probabilities(name: str, db: Session = Depends(get_db)):
     try:
         # 1. Appel à l'API publique
         nationalize_data = await fetch_nationalize_api_data(name)
 
         # 2. Remodelage des données
-        remodeled = remodel_data(nationalize_data)
+        remodeled = remodel_data(nationalize_data, db)
 
         # 3. Retourne le résultat
         return remodeled
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+app.include_router(country_routes)
--- back/api-name-origin-probabilities/src/api_name_origin_probabilities/services/api_nationalize.py
+++ back/api-name-origin-probabilities/src/api_name_origin_probabilities/services/api_nationalize.py
@@ -1,5 +1,8 @@
 import httpx
 from src.api_name_origin_probabilities.schemas import ApiNationalize, ApiNameOriginProbabilities
+from src.api_name_origin_probabilities.country import get_country_by_alias
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
 
 
 async def fetch_nationalize_api_data(name: str) -> ApiNationalize:
@@ -10,10 +13,18 @@ async def fetch_nationalize_api_data(name: str) -> ApiNationalize:
         return response.json()
 
 
-def remodel_data(original_data: ApiNationalize) -> ApiNameOriginProbabilities:
+def replace_alias_by_country_name(alias: str, db: Session) -> str:
+    try:
+        return get_country_by_alias(alias, db).name
+    except HTTPException:
+        print("Country not found in database")
+        return alias
+
+
+def remodel_data(original_data: ApiNationalize, db: Session) -> ApiNameOriginProbabilities:
     return ApiNameOriginProbabilities(
         probabilities=[{
-            "x": country["country_id"],
+            "x": replace_alias_by_country_name(country["country_id"], db),
             "y": round(country["probability"], 4)
         } for country in original_data["country"]]
     )
